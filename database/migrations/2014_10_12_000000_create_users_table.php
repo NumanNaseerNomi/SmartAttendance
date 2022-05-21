e7fc7a0da63943eaeae554e20d4191618265720e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-05-21 10:34:07 +0500

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('userName');
+            $table->string('about');
+            $table->string('cardId')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->boolean('isAdmin');
+            // $table->rememberToken();
+            // $table->timestamps();
         });
     }
 
--- database/migrations/2022_05_21_045228_create_attendance_table.php
+++ database/migrations/2022_05_21_045228_create_attendance_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('attendance', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->integer('userId');
+            $table->integer('deviceId');
+            $table->dateTime('checkIn', $precision = 0);
+            $table->dateTime('checkOut', $precision = 0)->nullable();
+            $table->boolean('isPresent');
         });
     }
 
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('attendance');
     }
 };
