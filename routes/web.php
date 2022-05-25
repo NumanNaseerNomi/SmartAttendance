be65205ab027496457558ef8ee68b514b5a79596
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-05-26 00:58:48 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+Route::get('/login', function () { return view('loginView'); });
+Route::get('/attendanceBook', function () { return view('attendanceBookView'); });
+Route::get('/manageUsers', function () { return view('manageUsersView'); });
+Route::get('/manageDevices', function () { return view('manageDevicesView'); });
