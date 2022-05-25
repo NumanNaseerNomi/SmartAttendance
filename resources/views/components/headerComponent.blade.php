919a41194d8d3ad689fb3473bc1fbd82a05b1cba
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-05-26 00:58:12 +0500

    Create headerComponent.blade.php

---
+++ resources/views/components/headerComponent.blade.php
@@ -0,0 +1,16 @@
+<head>
+    <title>Attendance System</title>
+    <!-- NOMi - Required meta tags - Start -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- NOMi - Required meta tags - End -->
+    <!-- NOMi - Bootstrap CSS - Start -->
+    <link href="plugins/bootstrap/bootstrap.min.css" rel="stylesheet" />
+    <!-- NOMi - Bootstrap CSS - End -->
+    <!-- NOMi - Font Awesome Icons CSS - Start -->
+    <link href="plugins/fontAwesome/css/fontAwesomeAll.min.css" rel="stylesheet" />
+    <!-- NOMi - Font Awesome Icons CSS - End -->
+    <!-- NOMi - Bootstrap Custom Dark Theme CSS - Start -->
+    <!-- <link href="css/bootstrapDarkTheme.min.css" rel="stylesheet" /> -->
+    <!-- NOMi - Bootstrap Custom Dark Theme  CSS - End -->
+</head>
