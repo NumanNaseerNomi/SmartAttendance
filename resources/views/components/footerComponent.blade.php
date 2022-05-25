cecc3a97e6f92757af010d3b50c242a58dfbf276
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-05-26 00:58:09 +0500

    Create footerComponent.blade.php

---
+++ resources/views/components/footerComponent.blade.php
@@ -0,0 +1,36 @@
+
+<!-- Footer - Start -->
+<footer>
+  <!-- NOMi - Bootstrap JavaScript File - Start -->
+  <script src="./plugins/bootstrap/bootstrap.bundle.min.js"></script>
+  <!-- NOMi - Bootstrap JavaScript File - End -->
+  <script>
+    // if (!localStorage.getItem("NOMi.theme"))
+    // {
+    //   localStorage.setItem("NOMi.theme", "dark");
+    // }
+
+    // function loadTheme(theme)
+    // {
+    //   document.querySelector(":root").setAttribute("color-scheme", theme);
+    //   document.querySelector("#themeChanger").value = theme;
+    // }
+
+    // function setTheme(theme)
+    // {
+    //   localStorage.setItem("NOMi.theme", theme);
+    //   loadTheme(theme);
+    // }
+
+    // function getCurrentTheme()
+    // {
+    //   return localStorage.getItem("NOMi.theme");
+    // }
+
+    // window.addEventListener("DOMContentLoaded", () => loadTheme(getCurrentTheme()));
+
+    // let themeChanger = document.querySelector("#themeChanger");
+    // themeChanger.addEventListener("change", () => setTheme(themeChanger.value));
+  </script>
+</footer>
+<!-- NOMi - Footer - End -->
