45b02cb1709246d7097ff1fc86ced7ebfbc2fccf
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-05-27 23:44:28 +0500

    Update headerComponent.blade.php

--- resources/views/components/headerComponent.blade.php
+++ resources/views/components/headerComponent.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- NOMi - Required meta tags - End -->
     <!-- NOMi - Bootstrap CSS - Start -->
-    <link href="plugins/bootstrap/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{ asset('plugins/bootstrap/bootstrap.min.css'); }}" rel="stylesheet" />
     <!-- NOMi - Bootstrap CSS - End -->
     <!-- NOMi - Font Awesome Icons CSS - Start -->
-    <link href="plugins/fontAwesome/css/fontAwesomeAll.min.css" rel="stylesheet" />
+    <link href="pluginsOld/fontAwesome/css/fontAwesomeAll.min.css" rel="stylesheet" />
     <!-- NOMi - Font Awesome Icons CSS - End -->
     <!-- NOMi - Bootstrap Custom Dark Theme CSS - Start -->
     <!-- <link href="css/bootstrapDarkTheme.min.css" rel="stylesheet" /> -->
