5bb6547186a44b6b1bec3d947dd49ed24184c70e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-05-29 16:29:47 +0500

    Update attendanceBookView.blade.php

--- resources/views/attendanceBookView.blade.php
+++ resources/views/attendanceBookView.blade.php
@@ -5,64 +5,14 @@
 		@include("components/navbarViewComponent")
 		<br/>
 		<div class="container">
-			<div class="row">
-				<div class="col-sm-12 col-md-3 mb-4">
-					<div class="card bg-light text-dark shadow">
-						<div class="card-header text-center"><h5>Filter Attendance</h5></div>
-						<div class="card-body">
-							<form>
-								<div class="mb-3">
-									<label for="userName" class="form-label">Select User</label>
-									<select class="form-select bg-white text-dark" aria-label="Default select example">
-										<option value="*" selected>All Users</option>
-										<option value="1">One</option>
-										<option value="2">Two</option>
-										<option value="3">Three</option>
-									</select>
-								</div>
-								<div class="mb-3">
-									<label for="cardID" class="form-label">Date From</label>
-									<input type="date" class="form-control bg-white text-dark" id="cardID">
-								</div>
-								<div class="mb-3">
-									<label for="cardID" class="form-label">Date To</label>
-									<input type="date" class="form-control bg-white text-dark" id="cardID">
-								</div>
-								<div class="mb-3">
-									<label for="cardID" class="form-label">Time In</label>
-									<input type="time" class="form-control bg-white text-dark" id="cardID">
-								</div>
-								<div class="mb-3">
-									<label for="cardID" class="form-label">Time Out</label>
-									<input type="time" class="form-control bg-white text-dark" id="cardID">
-								</div>
-								<div class="d-grid gap-2">
-									<button type="submit" class="btn btn-primary btn-sm">Filter</button>
-								</div>
-							</form>
-						</div>
+			<div class="card text-center">
+				<div class="card-header"><h5>Attendance Book</h5></div>
+					<div class="card-body">
+						<div id="example-table" class="overflow-auto"></div>
 					</div>
-				</div>
-				<div class="col-sm-12 col-md-9 mb-4">
-					<div class="card text-center bg-light shadow">
-						<div class="card-header"><h5>Attendance Book</h5></div>
-						<div class="container-fluid">
-							<div class="card-body table-responsive">
-								<table class="table table-bordered table-striped table-sm bg-light text-dark align-middle text-nowrap">
-									<thead class="align-middle">
-										<tr>
-											<th scope="col">#</th>
-											<th scope="col">USER NAME</th>
-											<th scope="col">CARD ID</th>
-											<th scope="col">ABOUT USER</th>
-											<th scope="col">Attendance</th>
-										</tr>
-									</thead>
-									<tbody id="attendanceBookTable"></tbody>
-								</table>
-							</dav>
-						</div>
-					</div>
+				<div class="card-footer text-muted">
+					<button class="btn btn-primary" type="button" id="printTable"><i class="fas fa-print">&emsp;</i>Print</button>
+					<button class="btn btn-primary" type="button" id="downloadTable"><i class="fas fa-file-code">&emsp;</i>Download as JSON</button>
 				</div>
 			</div>
 		</div>
@@ -71,33 +21,90 @@
 </html>
 
 <script>
-	function plotAttendanceTable(config)
-	{
-		let row = document.getElementById("attendanceBookTable").insertRow(0);
-		row.insertCell(0).innerHTML = config.srNumber;
-		row.insertCell(1).innerHTML = config.userName;
-		row.insertCell(2).innerHTML = config.cardId;
-		row.insertCell(3).innerHTML = config.aboutUser;
-		row.insertCell(4).innerHTML = config.dateTime;
-	}
+	// function plotAttendanceTable(config)
+	// {
+	// 	let row = document.getElementById("attendanceBookTable").insertRow(0);
+	// 	row.insertCell(0).innerHTML = config.srNumber;
+	// 	row.insertCell(1).innerHTML = config.userName;
+	// 	row.insertCell(2).innerHTML = config.cardId;
+	// 	row.insertCell(3).innerHTML = config.aboutUser;
+	// 	row.insertCell(4).innerHTML = config.dateTime;
+	// }
 	
-	fetch('/api/getAttendances')
-	.then((response) => response.json())
-	.then((respondedJsonData) =>
-	{
-		for (let i = 0; i < respondedJsonData.length; i++)
-		{
-			let dataConfig =
-			{
-				srNumber 	: i + 1,
-				userName 	: respondedJsonData[i].user.name + '<br/> (' + respondedJsonData[i].user.userName + ')',
-				cardId 		: respondedJsonData[i].user.cardId,
-				aboutUser 	: respondedJsonData[i].user.about,
-				dateTime 	: "<span class='text-success'>IN: " + respondedJsonData[i].attendance.checkIn +
-								"</span><br/> <span class='text-primary'>OUT: " + respondedJsonData[i].attendance.checkOut + "</span>"
-			};
+	// fetch('/api/getAttendances')
+	// .then((response) => response.json())
+	// .then((respondedJsonData) =>
+	// {console.log(respondedJsonData);
+	// 	for (let i = 0; i < respondedJsonData.length; i++)
+	// 	{
+	// 		let dataConfig =
+	// 		{
+	// 			srNumber 	: i + 1,
+	// 			userName 	: respondedJsonData[i].user.name + '<br/> (' + respondedJsonData[i].user.userName + ')',
+	// 			cardId 		: respondedJsonData[i].user.cardId,
+	// 			aboutUser 	: respondedJsonData[i].user.about,
+	// 			dateTime 	: "<span class='text-success'>IN: " + respondedJsonData[i].attendance.checkIn +
+	// 							"</span><br/> <span class='text-primary'>OUT: " + respondedJsonData[i].attendance.checkOut + "</span>"
+	// 		};
 			
-			plotAttendanceTable(dataConfig);
-		}
-	});
+	// 		plotAttendanceTable(dataConfig);
+	// 	}
+	// });
 </script>
+
+<script>
+// console.log(testData);
+// import TabulatorFull as Tabulator;
+//     define some sample data
+	// var tabledata =
+	// [
+	// 	{id:{a:1,b:1.1}, name:"Oli Bob", age:"12", col:"red", dob:""},
+	// 	{id:{a:2,b:2.2}, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
+	// 	{id:{a:3,b:3.3}, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
+	// 	{id:{a:4,b:4.4}, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
+	// 	{id:{a:5,b:5.5}, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+	// ];
+
+	// console.log(tabledata);
+
+	//create Tabulator on DOM element with id "example-table"
+	var table = new Tabulator(document.getElementById("example-table"),
+	{
+		// height:"311px",
+		layout:"fitColumns",
+		ajaxURL:"/api/getAttendances",
+		// progressiveLoad:"scroll",
+		// paginationSize:20,
+		placeholder:"No Record Found.",
+		// sortOrderReverse:true,
+		pagination: true,
+		paginationSize:10,
+		paginationSizeSelector:true,
+		paginationCounter:"rows",
+
+		// printAsHtml:true,
+		// printStyled:true,
+		printHeader:"<h1 class='text-center'>ATTENDANCE BOOK</h1>",
+
+		columns:
+		[
+			{title:"USER NAME", field:"user.userName", headerHozAlign:"center", hozAlign:"center", headerFilter:true, resizable:false, minWidth:150},
+			{title:"CARD ID", field:"user.cardId", headerHozAlign:"center", hozAlign:"center", headerFilter:true, resizable:false, minWidth:150},
+			{title:"ABOUT USER", field:"user.about", headerHozAlign:"center", hozAlign:"center", headerFilter:true, resizable:false, minWidth:150},
+			{title:"CHECK IN", field:"attendance.checkIn", headerHozAlign:"center", hozAlign:"center", headerFilter:true, resizable:false, minWidth:150},
+			{title:"CHECK OUT", field:"attendance.checkOut", headerHozAlign:"center", hozAlign:"center", headerFilter:true, resizable:false, minWidth:150},
+			{title:"Is Present?", field:"attendance.isPresent", headerHozAlign:"center", hozAlign:"center", headerFilter:"tickCross", formatter:"tickCross", headerFilterParams:{"tristate":true},headerFilterEmptyCheck:function(value){return value === null}, topCalc:"count"},
+			// {title:"Favourite Color", field:"col", sorter:"string"},
+			// {title:"Date Of Birth", field:"dob", sorter:"date", hozAlign:"center"},
+			// {title:"Driver", field:"car", hozAlign:"center", formatter:"tickCross", sorter:"boolean"},
+		],
+	});
+
+	document.getElementById("printTable").addEventListener("click", () => table.print(false, true));
+	document.getElementById("downloadTable").addEventListener("click", () => table.download("json", "AttendanceBook.json"));
+
+
+// 	var table = new Tabulator(document.getElementById("example-table"), {
+//     ajaxURL:"/api/getAttendances", //ajax URL
+// });
+</Script>
