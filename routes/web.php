239bfaa35b77d69a71200f4df8b3c73bf849abe8
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-05-31 08:32:58 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 Route::get('/', function () { return view('welcome'); });
-Route::get('/login', function () { return view('loginView'); });
 Route::get('/attendanceBook', function () { return view('attendanceBookView'); });
 Route::get('/manageUsers', function () { return view('manageUsersView'); });
 Route::get('/manageDevices', function () { return view('manageDevicesView'); });
+
+
+Route::get('/login', [AuthController::class, 'loginView']);
+Route::post('/loginAuth', [AuthController::class, 'loginAuth']);
+
+Route::get('/passwordReset', [AuthController::class, 'passwordResetView']);
+Route::post('/passwordResetAuth', [AuthController::class, 'passwordResetAuth']);
