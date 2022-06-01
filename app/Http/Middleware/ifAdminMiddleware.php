83789c7610a665c9d8aea9b093898cb011a07da2
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-06-01 23:45:46 +0500

    Create ifAdminMiddleware.php

---
+++ app/Http/Middleware/ifAdminMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Session;
+
+use Illuminate\Http\Request;
+
+class ifAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Session::get('user') && Session::get('user')->isAdmin)
+        {
+            // dd(Session::get('user')->isAdmin);
+            // return redirect('/');
+            return $next($request);
+        }
+        else
+        {
+            return redirect('/');
+        }
+        // return $next($request);
+    }
+}
