e8beb90e756ee0d32ef86595633a2459c484a342
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-06-04 22:53:25 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AttendanceBookController;
 
 Route::get('/', function () { return redirect('/attendanceBook'); })->middleware('Authen');
 
-Route::get('/attendanceBook', function () { return view('attendanceBookView'); })->middleware('Authen');
+// Route::get('/attendanceBook', function () { return view('attendanceBookView'); })->middleware('Authen');
+Route::get('/attendanceBook', [AttendanceBookController::class, 'attendanceBookView'])->middleware('Authen');
 Route::get('/manageUsers', function () { return view('manageUsersView'); })->middleware(['Authen', 'ifAdmin']);
 Route::get('/manageDevices', function () { return view('manageDevicesView'); })->middleware(['Authen', 'ifAdmin']);
 
