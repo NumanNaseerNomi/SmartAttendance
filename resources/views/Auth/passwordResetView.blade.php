d76b7998ee5322aac67f4412eef4ee30942298c4
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-06-26 22:19:26 +0500

    Update passwordResetView.blade.php

--- resources/views/Auth/passwordResetView.blade.php
+++ resources/views/Auth/passwordResetView.blade.php
@@ -1,38 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-	@include("components/headerComponent")
-	<body>
-		@include("components/navbarViewComponent")
-		<br/>
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-sm-12 col-md-6 col-lg-4">
-					<div class="card shadow">
-						<div class="card-header text-center"><h5>Change Pin Code</h5></div>
-						<div class="card-body">
-							<form method="post" action="{{ url('/passwordResetAuth') }}">
-								@csrf
-								<div class="mb-3">
-									<label for="currentPinCode" class="form-label">Enter Current Pin Code</label>
-									<input type="password" class="form-control" name="currentPinCode" id="currentPinCode" required>
-								</div>
-								<div class="mb-3">
-									<label for="newPinCode" class="form-label">Enter New Pin Code</label>
-									<input type="password" class="form-control" name="newPinCode" id="newPinCode" required>
-								</div>
-								<div class="mb-3">
-									<label for="newPinCode_confirmation" class="form-label">Confirm New Pin Code</label>
-									<input type="password" class="form-control" name="newPinCode_confirmation" id="newPinCode_confirmation" required>
-								</div>
-								<div class="d-grid gap-2">
-									<button type="submit" class="btn btn-primary btn-sm">Save</button>
-								</div>
-							</form>
+
+@include("components/headerComponent")
+@include("components/navbarViewComponent")
+<br/>
+<div class="container">
+	<div class="row justify-content-center">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+		<div class="col-sm-12 col-md-6 col-lg-4">
+			<div class="card shadow">
+				<div class="card-header text-center"><h5>Change Password</h5></div>
+				<div class="card-body">
+					<form method="post" action="{{ url('/passwordResetAuth') }}">
+						@csrf
+						<div class="mb-3">
+							<label for="current_password" class="form-label">Enter Old Password</label>
+							<input type="password" class="form-control" name="current_password" required>
 						</div>
-					</div>
+						<div class="mb-3">
+							<label for="password" class="form-label">Enter New Password</label>
+							<input type="password" class="form-control" name="password" required>
+						</div>
+						<div class="mb-3">
+							<label for="password_confirmation" class="form-label">Confirm New Password</label>
+							<input type="password" class="form-control" name="password_confirmation" required>
+						</div>
+						<div class="d-grid gap-2">
+							<button type="submit" class="btn btn-primary btn-sm">Set Password</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
-		@include("components/footerComponent")
-	</body>
-</html>
+	</div>
+</div>
+@include("components/footerComponent")
