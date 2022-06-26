5c770f69d569a5343c7d868066d66d0f44969f25
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-06-26 22:17:47 +0500

    Update footerComponent.blade.php

--- resources/views/components/footerComponent.blade.php
+++ resources/views/components/footerComponent.blade.php
@@ -1,39 +1,7 @@
 
-<!-- Footer - Start -->
-<footer>
-  <!-- NOMi - Bootstrap JavaScript File - Start -->
-  <script src="{{ asset('/plugins/bootstrap/bootstrap.bundle.min.js') }}"></script>
-  <!-- NOMi - Bootstrap JavaScript File - End -->
-  <!-- NOMi - Tabulator JavaScript File - Start -->
-  <script src="{{ asset('/plugins/tabulator/tabulator.min.js') }}"></script>
-  <!-- NOMi - Tabulator JavaScript File - End -->
-  <script>
-    // if (!localStorage.getItem("NOMi.theme"))
-    // {
-    //   localStorage.setItem("NOMi.theme", "dark");
-    // }
-
-    // function loadTheme(theme)
-    // {
-    //   document.querySelector(":root").setAttribute("color-scheme", theme);
-    //   document.querySelector("#themeChanger").value = theme;
-    // }
-
-    // function setTheme(theme)
-    // {
-    //   localStorage.setItem("NOMi.theme", theme);
-    //   loadTheme(theme);
-    // }
-
-    // function getCurrentTheme()
-    // {
-    //   return localStorage.getItem("NOMi.theme");
-    // }
-
-    // window.addEventListener("DOMContentLoaded", () => loadTheme(getCurrentTheme()));
-
-    // let themeChanger = document.querySelector("#themeChanger");
-    // themeChanger.addEventListener("change", () => setTheme(themeChanger.value));
-  </script>
-</footer>
-<!-- NOMi - Footer - End -->
+    <footer>
+      <script src="{{ asset('/plugins/bootstrap/bootstrap.bundle.min.js') }}"></script>
+      <script src="{{ asset('/plugins/tabulator/tabulator.min.js') }}"></script>
+    </footer>
+  </body>
+</html>
