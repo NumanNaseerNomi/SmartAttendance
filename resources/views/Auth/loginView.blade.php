aab7b379136d1018fbffec8ebc741753583b161e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-06-26 22:24:31 +0500

    Update loginView.blade.php

--- resources/views/Auth/loginView.blade.php
+++ resources/views/Auth/loginView.blade.php
@@ -1,34 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-	@include("components/headerComponent")
-	<body>
-		@include("components/navbarViewComponent")
-		<br/>
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-sm-12 col-md-6 col-lg-4">
-					<div class="card shadow">
-						<div class="card-header text-center"><h5>Login</h5></div>
-						<div class="card-body">
-							<form method="post" action="{{ url('/login') }}">
-								@csrf
-								<div class="mb-3">
-									<label for="userName" class="form-label">Enter User Name</label>
-									<input type="text" class="form-control" name="userName" id="userName" required>
-								</div>
-								<div class="mb-3">
-									<label for="pinCode" class="form-label">Enter Pin Code</label>
-									<input type="password" class="form-control" name="pinCode" id="pinCode" required>
-								</div>
-								<div class="d-grid gap-2">
-									<button type="submit" class="btn btn-primary btn-sm">Login</button>
-								</div>
-							</form>
+
+@include("components/headerComponent")
+@include("components/navbarViewComponent")
+<br/>
+<div class="container">
+	<div class="row justify-content-center">
+		@if ($errors->any())
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+						<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+		@endif
+		<div class="col-sm-12 col-md-6 col-lg-4">
+			<div class="card shadow">
+				<div class="card-header text-center"><h5>Login</h5></div>
+				<div class="card-body">
+					<form method="post" action="{{ url('/login') }}">
+						@csrf
+						<div class="mb-3">
+							<label for="userName" class="form-label">Enter User Name</label>
+							<input type="text" class="form-control" name="userName" required>
 						</div>
-					</div>
+						<div class="mb-3">
+							<label for="password" class="form-label">Enter Pin Code</label>
+							<input type="password" class="form-control" name="password" required>
+						</div>
+						<div class="d-grid gap-2">
+							<button type="submit" class="btn btn-primary btn-sm">Login</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
-		@include("components/footerComponent")
-	</body>
-</html>
+	</div>
+</div>
+@include("components/footerComponent")
