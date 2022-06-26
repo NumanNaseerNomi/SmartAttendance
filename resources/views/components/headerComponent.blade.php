f28e8ff9c80e18013a22168e75d2f62839621852
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-06-26 22:17:37 +0500

    Update headerComponent.blade.php

--- resources/views/components/headerComponent.blade.php
+++ resources/views/components/headerComponent.blade.php
@@ -1,19 +1,11 @@
-<head>
-    <title>Attendance System</title>
-    <!-- NOMi - Required meta tags - Start -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <!-- NOMi - Required meta tags - End -->
-    <!-- NOMi - Bootstrap CSS - Start -->
-    <link href="{{ asset('plugins/bootstrap/bootstrap.min.css') }}" rel="stylesheet" />
-    <!-- NOMi - Bootstrap CSS - End -->
-    <!-- NOMi - Tabulator CSS - Start -->
-    <link href="{{ asset('plugins/tabulator/tabulator.min.css') }}" rel="stylesheet" />
-    <!-- NOMi - Tabulator CSS - End -->
-    <!-- NOMi - Font Awesome Icons CSS - Start -->
-    <link href="pluginsOld/fontAwesome/css/fontAwesomeAll.min.css" rel="stylesheet" />
-    <!-- NOMi - Font Awesome Icons CSS - End -->
-    <!-- NOMi - Bootstrap Custom Dark Theme CSS - Start -->
-    <!-- <link href="css/bootstrapDarkTheme.min.css" rel="stylesheet" /> -->
-    <!-- NOMi - Bootstrap Custom Dark Theme  CSS - End -->
-</head>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Attendance System</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="{{ asset('plugins/bootstrap/bootstrap.min.css') }}" rel="stylesheet" />
+        <link href="{{ asset('plugins/tabulator/tabulator.min.css') }}" rel="stylesheet" />
+        <link href="pluginsOld/fontAwesome/css/fontAwesomeAll.min.css" rel="stylesheet" />
+    </head>
+    <body>
