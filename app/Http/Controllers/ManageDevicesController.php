71e1a3a47b2fa73e43cc6277206329637a68e4e6
NOMi <numan.naseer.nomi@gmail.com>
2022-06-27 18:43:42 +0500

    Create ManageDevicesController.php

---
+++ app/Http/Controllers/ManageDevicesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\UsersModel;
+
+class ManageDevicesController extends Controller
+{
+    function manageDevicesView()
+    {
+        $usersDetail = UsersModel::all();
+        return view('manageDevicesView')->with(['usersDetail' => $usersDetail]);
+    }
+
+    function saveUser(Request $request)
+    {
+        $request->validate(
+            [
+                '_token' => 'required',
+                'name' => 'required',
+                'cardId' => 'required',
+                'about' => 'required',
+                'isBlocked' => 'required'
+            ]
+        );
+
+        if($request->id)
+        {
+            $result = UsersModel::find($request->id);
+            $result->update($request->all());
+        }
+        else
+        {
+            $userName = str_replace(' ', '.', strtolower($request->name)) . "." . rand(1,999);
+            $request->merge(['userName' => $userName, 'password' => Hash::make('admin'), 'isAdmin' => 0]);
+            UsersModel::create($request->all());
+        }
+        
+        return redirect()->back();
+    }
+}
