4f5c231e18c48b91be79e309354e6b6300261f86
NOMi <numan.naseer.nomi@gmail.com>
2022-06-28 13:46:31 +0500

    Update headerComponent.blade.php

--- resources/views/components/headerComponent.blade.php
+++ resources/views/components/headerComponent.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="{{ asset('plugins/bootstrap/bootstrap.min.css') }}" rel="stylesheet" />
-        <link href="{{ asset('plugins/fontawesome/css/fontawesome.css') }}" rel="stylesheet" />
+        <link href="{{ asset('plugins/fontawesome/fontawesome.css') }}" rel="stylesheet" />
         <link href="{{ asset('plugins/tabulator/tabulator.min.css') }}" rel="stylesheet" />
     </head>
     <body>
