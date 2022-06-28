36fcd996e47c738195292eda1de511b9893f84d8
NOMi <numan.naseer.nomi@gmail.com>
2022-06-28 14:41:22 +0500

    Update main.cpp

--- NodeMCU/SmartAttendance/src/main.cpp
+++ NodeMCU/SmartAttendance/src/main.cpp
@@ -8,7 +8,7 @@
 #define WiFiSSID      "Tenda_D343D7"
 #define WiFiPassword  "123456780"
 
-#define DeviceToken "26452NOMi"
+#define DeviceId "26452NOMi"
 
 #define URLPath  "http://192.168.1.2/SmartAttendance/public/api/markAttendance"
 
@@ -82,7 +82,7 @@ void loop()
     lcd.print("RFID: ");
     lcd.print(rfidTagID);
     
-    #define Parameters "?cardId="+String(rfidTagID)+"&deviceToken="+String(DeviceToken)
+    #define Parameters "?cardId="+String(rfidTagID)+"&deviceId="+String(DeviceId)
     String fullURL = URLPath Parameters;
     // Serial.println(fullURL);
     
