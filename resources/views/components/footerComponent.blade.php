e47ae5a141c34e8b9e24e5de58b611f8e05e666e
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-08-31 22:59:54 +0500

    removed old version

--- resources/views/components/footerComponent.blade.php
+++
@@ -1,7 +0,0 @@
-
-    <footer>
-      <script src="{{ asset('/plugins/bootstrap/bootstrap.bundle.min.js') }}"></script>
-      <script src="{{ asset('/plugins/tabulator/tabulator.min.js') }}"></script>
-    </footer>
-  </body>
-</html>
