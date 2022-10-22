619c4f2dccfb503dba85150ddf6fe4f70679ad25
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-23 04:50:40 +0500

    Update SaveRecord.vue

--- resources/views/components/SaveRecord.vue
+++ resources/views/components/SaveRecord.vue
@@ -2,36 +2,33 @@
 <div class="card bg-light text-dark shadow">
 	<div class="card-header text-center"><h5>Add New {User/Device}</h5></div>
         <div class="card-body">
-            <form>
+            <form @submit.prevent="saveRecord()">
                 <div class="mb-3">
                     <label class="form-label">Serial Number</label>
-                    <input class="form-control form-control-sm" v-model="record.isAdmin" readonly required />
+                    <input class="form-control form-control-sm" v-model="record.id" readonly required />
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Name *</label>
                     <input class="form-control form-control-sm" v-model="record.name" required />
                 </div>
                 <div class="mb-3">
-                    <!-- <label for="{{ strtolower($idType) }}Id" class="form-label">{{ $idType }} ID</label>
-                    <input type="text" class="form-control form-control-sm" id="{{ strtolower($idType) }}Id" name="{{ strtolower($idType) }}Id" required> -->
                     <label class="form-label">{Card/Device} ID *</label>
                     <input class="form-control form-control-sm" v-model="record.itemId" required />
                 </div>
                 <div class="mb-3">
-                    <label for="description" class="form-label">Description</label>
-                    <input type="text" class="form-control form-control-sm" id="description" name="description" required>
+                    <label class="form-label">Description *</label>
+                    <input class="form-control form-control-sm" v-model="record.description" required />
                 </div>
                 <div class="mb-3">
-                    <label for="status" class="form-label">Status</label>
-                    <select class="form-select form-select-sm" id="status" name="isBlocked" required>
-                        <option disabled selected></option>
+                    <label class="form-label">Status *</label>
+                    <select class="form-select form-select-sm" v-model="record.status" required>
                         <option value="0">Active</option>
                         <option value="1">Block</option>
                     </select>
                 </div>
                 <div class="row">
                     <div class="d-grid gap-2 col-6">
-                        <button type="reset" class="btn btn-outline-primary btn-sm">Reset</button>
+                        <button type="reset" @click.prevent="record = {}" class="btn btn-outline-primary btn-sm">Reset</button>
                     </div>
                     <div class="d-grid gap-2 col-6">
                         <button type="submit" class="btn btn-outline-success btn-sm">Save</button>
@@ -49,11 +46,23 @@
         {
             let data =
             {
-                record: {id:545, name:"Numan Naseer Nomi", itemId:"numan.naseer.nomi", isAdmin:1},
-                // record: null
+                record: {id:545, name:"Numan Naseer Nomi", itemId:"numan.naseer.nomi", description:"sdf sd", status:0},
+                // record: {}
             };
 
             return data;
+        },
+
+        methods:
+        {
+            saveRecord()
+            {
+                console.log(this.record);
+                this.record = {};
+                fetch('https://jsonplaceholder.typicode.com/todos/1')
+                .then(response => response.json())
+                .then(json => console.log(json))
+            }
         }
     }
 </script>
