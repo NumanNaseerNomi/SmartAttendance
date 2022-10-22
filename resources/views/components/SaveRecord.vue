8479eca50b353e71c792048c58531c0e9e74513a
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-23 03:39:19 +0500

    Create SaveRecord.vue

---
+++ resources/views/components/SaveRecord.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="card bg-light text-dark shadow">
+	<div class="card-header text-center"><h5>Add New {{ $type }}</h5></div>
+        <div class="card-body">
+            <form method="post" action="{{ url('/save' . $type) }}">
+                @csrf
+                <div class="mb-3">
+                    <label for="id" class="form-label">Serial Number</label>
+                    <input type="text" class="form-control form-control-sm" id="id" name="id" readonly required>
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control form-control-sm" id="name" name="name" required>
+                </div>
+                <div class="mb-3">
+                    <label for="{{ strtolower($idType) }}Id" class="form-label">{{ $idType }} ID</label>
+                    <input type="text" class="form-control form-control-sm" id="{{ strtolower($idType) }}Id" name="{{ strtolower($idType) }}Id" required>
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <input type="text" class="form-control form-control-sm" id="description" name="description" required>
+                </div>
+                <div class="mb-3">
+                    <label for="status" class="form-label">Status</label>
+                    <select class="form-select form-select-sm" id="status" name="isBlocked" required>
+                        <option disabled selected></option>
+                        <option value="0">Active</option>
+                        <option value="1">Block</option>
+                    </select>
+                </div>
+                <div class="row">
+                    <div class="d-grid gap-2 col-6">
+                        <button type="reset" class="btn btn-outline-primary btn-sm">Reset</button>
+                    </div>
+                    <div class="d-grid gap-2 col-6">
+                        <button type="submit" class="btn btn-outline-success btn-sm">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default
+    {
+        data()
+        {
+            let data =
+            {
+                authUser: {name:"Numan Naseer Nomi", userName:"numan.naseer.nomi", isAdmin:1},
+                // authUser: null
+            };
+
+            return data;
+        }
+    }
+</script>
