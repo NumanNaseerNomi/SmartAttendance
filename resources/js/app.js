02b56f5edb0211e591510d232b3778df877d7160
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-23 01:30:36 +0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import "./bootstrap";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 import { createApp } from "vue";
 import app from "../views/app.vue";
