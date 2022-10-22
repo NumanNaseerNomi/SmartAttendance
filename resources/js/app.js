99f7667aaab1a11001d56bf2f8a45995f75e337d
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-22 18:56:51 +0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import "./bootstrap";
-import { createApp } from "vue";
-import app from "../views/app.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import { createApp } from "vue";
+import app from "../views/app.vue";
 
 createApp(app).mount("#app");
