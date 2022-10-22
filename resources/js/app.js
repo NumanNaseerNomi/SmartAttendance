0d789dcafea46c3f80618f5cafa908f79a2339ec
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-22 18:52:33 +0500

    Bootstrap added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import "./bootstrap";
 import { createApp } from "vue";
 import app from "../views/app.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createApp(app).mount("#app");
