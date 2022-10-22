90594b366aebfeff2a3a662772e05e192997304d
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-23 02:27:53 +0500

    Update TopNavbar.vue

--- resources/views/components/TopNavbar.vue
+++ resources/views/components/TopNavbar.vue
@@ -1,34 +1,36 @@
 <template>
     <header class="mb-5">
-        <nav class="navbar fixed-top bg-light shadow py-1">
+        <nav class="navbar fixed-top shadow py-1">
             <div class="container-fluid">
                 <!-- NOMi - Side Navigation Bar - Start -->
                 <div class="dropdown">
                     <!-- @if(Session::get('user')) -->
-                        <button class="btn btn-link text-dark py-0" title="Menu" type="button" id="menu" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-bars fs-4"></i></button>
-                        <div class="dropdown-menu dropdown-menu-start shadow bg-light" aria-labelledby="menu">
-                            <a class="dropdown-item text-dark my-2" href="/attendanceBook'">
+                        <button class="btn btn-link text-dark py-0" title="Menu" type="button" id="menu" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fas fa-bars fs-4"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-start shadow" aria-labelledby="menu">
+                            <a class="dropdown-item my-2" href="/attendanceBook'">
                                 <div class="row flex-nowrap">
                                     <div class="col-2 text-center"><i class="fas fa-clipboard-check"></i></div>
                                     <div class="col-10">Attendance Book</div>
                                 </div>
                             </a>
                             <!-- @if(Session::get('user')->isAdmin) -->
-                                <a class="dropdown-item text-dark my-2" href="/manageUsers">
+                                <a class="dropdown-item my-2" href="/manageUsers">
                                     <div class="row flex-nowrap">
                                         <div class="col-2 text-center"><i class="fas fa-users"></i></div>
                                         <div class="col-10">Manage Users</div>
                                     </div>
                                 </a>
-                                <a class="dropdown-item text-dark my-2" href="/manageDevices">
+                                <a class="dropdown-item my-2" href="/manageDevices">
                                     <div class="row flex-nowrap">
                                         <div class="col-2 text-center"><i class="fas fa-satellite"></i></div>
                                         <div class="col-10">Manage Devices</div>
                                     </div>
                                 </a>
                             <!-- @endif -->
-                            <hr class="dropdown-divider border-secondary"/>
-                            <p class="small m-0 text-center text-dark">
+                            <hr class="dropdown-divider"/>
+                            <p class="small m-0 text-center">
                                 <span>Powered by </span>
                                 <a class="text-decoration-none text-dark" href="https://www.fb.com/numan.naseer.nomi" target="_blank">
                                     <strong>NOMi</strong>
@@ -48,7 +50,9 @@
                     <div class="btn-group">
                         <div class="dropdown">
                             <!-- @if(Session::get('user')) -->
-                                <button class="btn btn-link text-dark py-0" title="My Profile" type="button" id="menuDropdownButton" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fs-4"></i></button>
+                                <button class="btn btn-link text-dark py-0" title="My Profile" type="button" id="menuDropdownButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="fas fa-user fs-4"></i>
+                                </button>
                                 <div class="dropdown-menu dropdown-menu-end shadow bg-light" aria-labelledby="menuDropdownButton">
                                     <span class="dropdown-item text-dark my-2 text-center" title="My Profile">
                                         <!-- <div>{{Session::get('user')->name}}</div>
