c896da9fcbba973b7058de00ffdd3f35deccff41
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-24 01:32:46 +0500

    Update LoginPage.vue

--- resources/views/components/auth/LoginPage.vue
+++ resources/views/components/auth/LoginPage.vue
@@ -14,18 +14,17 @@
 			<div class="card shadow">
 				<div class="card-header text-center"><h5>Login</h5></div>
 				<div class="card-body">
-					<form method="post" action="{{ url('/login') }}">
-						<!-- @csrf -->
+					<form @submit.prevent="login()">
 						<div class="mb-3">
-							<label for="userName" class="form-label">Enter User Name</label>
-							<input type="text" class="form-control" name="userName" required>
+							<label class="form-label">Enter User Name</label>
+                            <input class="form-control" v-model="records[0].id" required />
 						</div>
 						<div class="mb-3">
-							<label for="password" class="form-label">Enter Password</label>
-							<input type="password" class="form-control" name="password" required>
+							<label class="form-label">Enter Password</label>
+                            <input class="form-control" type="password" v-model="records[0].id" required />
 						</div>
 						<div class="d-grid gap-2">
-							<button type="submit" class="btn btn-primary btn-sm">Login</button>
+							<button type="submit" class="btn btn-outline-primary">Login</button>
 						</div>
 					</form>
 				</div>
