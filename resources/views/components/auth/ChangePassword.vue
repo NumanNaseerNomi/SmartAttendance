9a07fc7564e57da3742a7d3c9e8dad513d969439
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-10-25 01:27:21 +0500

    Update ChangePassword.vue

--- resources/views/components/auth/ChangePassword.vue
+++ resources/views/components/auth/ChangePassword.vue
@@ -3,16 +3,20 @@
         <div class="row justify-content-center">
             <div class="col-sm-12 col-md-6 col-lg-4">
                 <div class="card shadow">
-                    <div class="card-header text-center"><h5>Login</h5></div>
+                    <div class="card-header text-center"><h5>Change Password</h5></div>
                     <div class="card-body">
-                        <form @submit.prevent="login()">
+                        <form @submit.prevent="changePassword()">
                             <div class="mb-3">
-                                <label class="form-label">Enter User Name</label>
-                                <input class="form-control" v-model="authUser.userName" required />
+                                <label class="form-label">Enter Old Password</label>
+                                <input class="form-control" type="password" v-model="password.old" required />
                             </div>
                             <div class="mb-3">
-                                <label class="form-label">Enter Password</label>
-                                <input class="form-control" type="password" v-model="authUser.password" required />
+                                <label class="form-label">Enter New Password</label>
+                                <input class="form-control" type="password" v-model="password.new" required />
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Renter New Password</label>
+                                <input class="form-control" type="password" v-model="password.confirm" required />
                             </div>
                             <div class="mb-3">
                                 <div class="alert alert-danger" v-if="errors">
@@ -20,7 +24,7 @@
                                 </div>
                             </div>
                             <div class="d-grid gap-2">
-                                <button type="submit" class="btn btn-outline-primary">Login</button>
+                                <button type="submit" class="btn btn-outline-primary">Save</button>
                             </div>
                         </form>
                     </div>
@@ -37,13 +41,7 @@
                 {
                     let data =
                     {
-                        authUser: {},
-    
-                        // errors:
-                        // {
-                        //     userName:"numan.naseer.nomi",
-                        //     password:"sdfsdasd fasdfasdf asdf a"
-                        // },
+                        password: {},
                         errors: null,
                     };
         
@@ -54,12 +52,12 @@
                 {
                     
     
-                    login()
+                    changePassword()
                     {
                         let payload =
                         {
-                            "email": this.authUser.userName, // eve.holt@reqres.in
-                            "password": this.authUser.password, // "cityslicka"
+                            "email": "eve.holt@reqres.in",
+                            "password": "cityslicka",
                         };
         
                         let apiRequestDetails =
@@ -71,23 +69,23 @@
         
                         fetch("https://reqres.in/api/login", apiRequestDetails)
                         .then((response) => response.json())
-                        .then((json) => this.authUserInfo(json))
+                        .then((json) => this.handleResponse(json))
                         .catch((error) => console.warn(error));
                     },
     
-                    authUserInfo(userInfo)
+                    handleResponse(userInfo)
                     {
-                        this.authUser =
-                        {
-                            userName: this.authUser.userName,
-                            apiToken: userInfo.token,
-                        }
+                        // this.authUser =
+                        // {
+                        //     userName: this.authUser.userName,
+                        //     apiToken: userInfo.token,
+                        // }
                         // // console.log(userInfo);
     
                         // let myObj = { name: 'Skip', breed: 'Labrador' };
-                        localStorage.setItem("authUser", JSON.stringify(this.authUser));
+                        // localStorage.setItem("authUser", JSON.stringify(this.password));
     
-                        this.errors = this.authUser;
+                        this.errors = this.password;
     
                         // let item = JSON.parse(localStorage.getItem("authUser"));
                         // console.log(this.authUser);
